f662b1515fbd83393f4c49fbc44c673ccd43756c
sergiioo101 <smartros@myuax.com>
2024-05-03 18:09:31 +0200

    1

---
+++ src/main/java/model/Poblacion.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Poblacion {
+    private String nombre;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private int numBacterias;
+    private double temperatura;
+    private String luminosidad;
+    private int comidaInicial;
+    private int diaIncremento;
+    private int comidaMaxima;
+    private int comidaFinal;
+    private int[] planAlimentacion;
+}
